945cbf245355c2c0fba4e16dcfa02da820ca0e23
jules <invertime.off@gmail.com>
2022-02-06 01:01:00 +0100

    update on dashboard

---
+++ dashboard/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Photos", icon: "mdi-image" },
+        { title: "About", icon: "mdi-help-box" },
+      ],
+      right: null,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ dashboard/src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card">
+    <div class="header">
+      <div class="icon"></div>
+      <div class="text">
+        <div class="title"></div>
+        <div class="subtitle"></div>
+      </div>
+    </div>
+    <div class="body">
+      <div class="temperature"></div>
+      <div class="pressure"></div>
+      <div class="humidity"></div>
+    </div>
+  </div>
+  <div class="card"></div>
+  {{ values }}
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    values: null,
+    loading: false,
+  }),
+  mounted() {
+    this.loading = true;
+    axios
+      .get("https://reqres.in/api/users?page=1")
+      .then((response) => (this.values = response.data.data))
+      .catch((error) => console.log(error))
+      .finally(() => (this.loading = false));
+  },
+};
+</script>
